d6c5d6f5a72d47ec90e8de4ce95a8adea51e4a85
skimok <skim@hknu.ac.kr>
2024-07-19 07:02:42 +0900

    Add files via upload

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -3,17 +3,7 @@ const mongoose = require('mongoose');
 const readLine = require('readline');
 mongoose.set("strictQuery", false);
 
-// const dbPassword = process.env.MONGODB_PASSWORD;
 const dbURI = `mongodb+srv://my_atlas_user:tkarms77@cluster0.s0fko.mongodb.net/Loc8r`;
-
-// mongoose.connect(process.env.MONGODB_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// }).then(() => {
-//   console.log('MongoDB connected');
-// }).catch((error) => {
-//   console.error('MongoDB connection error:', error);
-// });
 
 const connect = () => {
   setTimeout(() => mongoose.connect(dbURI, { useNewUrlParser: true }), 1000);
